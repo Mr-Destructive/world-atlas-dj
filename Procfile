899011ad5cc68cfb6316601a1b469a4b6522b4b5
Mr-Destructive <gormeet711@gmail.com>
2021-12-31 21:54:19 +0530

    client scripts rewritten

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-release: python manage.py migrate --noinput
-web: daphne WorldAtlas.asgi:application --port $PORT --bind 0.0.0.0
-worker: python manage.py runworker channels --settings=WorldAtlas.settings -v2
+web: gunicorn WorldAtlas.wsgi
--- WorldAtlas/asgi.py
+++ WorldAtlas/asgi.py
@@ -1,30 +1,6 @@
-"""
-ASGI config for WorldAtlas project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-
-from django.conf.urls import url
 from django.core.asgi import get_asgi_application
-import room.routing
-# Fetch Django ASGI application early to ensure AppRegistry is populated
-# before importing consumers and AuthMiddlewareStack that may import ORM
-# models.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WorldAtlas.settings")
-django_asgi_app = get_asgi_application()
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            room.routing.websocket_urlpatterns
-        )
-    ),
-})
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WorldAtlas.settings")
+application = get_asgi_application()
+
